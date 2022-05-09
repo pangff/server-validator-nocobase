1db4c0a4aec521bf6e8cd079e4864582babc10b7
pangff <pangff@gmail.com>
2022-05-09 19:32:54 +0800

    添加test

---
+++ src/__test__/index.test.ts
@@ -0,0 +1,14 @@
+/*
+ * @Author: pangff
+ * @Date: 2022-05-09 19:31:25
+ * @LastEditTime: 2022-05-09 19:32:41
+ * @LastEditors: pangff
+ * @Description: 
+ * @FilePath: /server-validator-nocobase/src/__test__/index.test.ts
+ * stay hungry,stay foolish
+ */
+describe("app test", () => {
+    it("test", async () => {
+       
+    });
+});
